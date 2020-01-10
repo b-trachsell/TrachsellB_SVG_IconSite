087cc4d35b77009d521087a88bdfe1d8e380d7bd
Trachsell <b_trachsell@ats-hr420-dm35.fc.ca>
2020-01-10 14:38:30 -0500

    Added SVG and Hover listener

--- js/main.js
+++ js/main.js
@@ -3,23 +3,34 @@
 // also called a module
 // A function with no name
 
+// Lets and then Functions and then Events
+
 (() => {
   console.log(" Some Message ");
 
 
   // Finds the button element on the page by its id with javascript
   // Lets your javascript know what the button is
-  let theButton = document.querySelector("#myButton");
+  let theButton = document.querySelector("#myButton"),
+      theHeading = document.querySelector('h1'),
+      svgImage = document.querySelector("#svgGraphic");
+
 
 
 
 // Creates the function to change the text
   function changeText() {
-    document.querySelector('h1').textContent = "hello world";
+    theHeading.textContent = "hello world";
+  }
+
+  function logSVG() {
+    console.log(this.id);
   }
 
 // Calls the function when the button is clicked
+// Usually mouse clicking or Hovers
   theButton.addEventListener("click", changeText);
+  svgImage.addEventListener("mouseover", logSVG);
 
 
 })();
